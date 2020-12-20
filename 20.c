33cf95bd0c8a37261e64e7fd56c3cfde8db28f70
metacolon <timo@teemze.de>
2020-12-20 13:27:42 +0100

    Remove unused prints

--- 20.c
+++ 20.c
@@ -24,18 +24,6 @@ int main()
 	FILE *in = fopen("in20", "r");
 
     Tile *parsed = parse(in);
-    /*for (int i = 0; i < TILE_CNT; i++)
-    {
-        printf("Tile %ld:\n", parsed[i].id);
-        for (int j = 0; j < SIZE; j++)
-        {
-            for (int k = 0; k < SIZE; k++)
-                printf("%c", parsed[i].image[j*SIZE+k] ? '#' : '.');
-            printf("\n");
-        }
-        printf("\n");
-    }*/
-    //printf("Parsed\n");
     long part1 = 1;
     Tile *corners = getCorners(parsed);
     for (int i = 0; i < 4; i++)
@@ -69,10 +57,7 @@ int getMatchingCount(Tile *all, int one)
     for (int i = 0; i < TILE_CNT; i++)
         if (one != i)
         {
-            //printf("It's a potential match! (%ld - %ld; %d - %d)\n", all[one].id, all[i].id, one, i);
             int m = matches(all[one], all[i]);
-            //if (m)
-                //printf("%ld matches %ld (%d - %d)\n", all[one].id, all[i].id, one, i);
             res += m;
         }
     return res;
@@ -80,30 +65,18 @@ int getMatchingCount(Tile *all, int one)
 
 int matchLine (char *a, char *b)
 {
-    //printf("Matching lines...\n");
     // In order
     int i;
     for (i = 0; i < SIZE; i++)
-    {
-        //printf("%d...\n", i);
         if (a[i] != b[i])
             break;
-    }
     if(i == SIZE)
-    {
-        //printf("Matched\n");
         return 1;
-    }
-    //printf("Reverse...\n");
     for (i = 0; i < SIZE; i++)
         if (a[i] != b[SIZE-i-1])
             break;
     if (i == SIZE)
-    {
-        //printf("Matched\n");
         return 1;
-    }
-    //printf("Didn't match\n");
     return 0;
 }
 char **getBorders(char *arr)
@@ -132,18 +105,13 @@ int matches(Tile a, Tile b)
     char **bordersA = getBorders(a.image);
     char **bordersB = getBorders(b.image);
     int res = 0;
-    //printf("Got border of %ld and %ld\n", a.id, b.id);
     for (int i = 0; i < 4; i++)
         for(int j = 0; j < 4; j++)
-        {
-            //printf("Matching borders %d and %d\n", i, j);
             if (matchLine(bordersA[i], bordersB[j]))
             {
-                //printf("It's a match! (%ld - %ld)\n", a.id, b.id);
                 res = 1;
                 goto end;
             }
-        }
 end:
     for (int i = 0; i < 4; i++)
     {
@@ -171,15 +139,6 @@ Tile *parse(FILE *in)
         if (*line == '\n')
         {
             // Tile done
-            /*printf("Tile %ld:\n", currentTile.id);
-            for (int j = 0; j < SIZE; j++)
-            {
-                for (int k = 0; k < SIZE; k++)
-                    printf("%c", currentTile.image[j*SIZE+k] ? '#' : '.');
-                printf("\n");
-            }
-            printf("\n");*/
-
             res[resIndex++] = currentTile;
             memset(&currentTile, 0, sizeof(currentTile));
             currentTileLine = 0;
@@ -188,19 +147,16 @@ Tile *parse(FILE *in)
         if (strstr(line, "Tile") != NULL)
         {
             // Header
-            //printf("Got header\n");
             *strchr(line, ':') = '\0';
             currentTile.id = atol(strchr(line, ' ')+1);
             continue;
         }
 
         parseLine(line);
-        //printf("Parsed line (%d)\n", currentTileLine);
         for (int i = 0; i < SIZE; i++)
             currentTile.image[currentTileLine * SIZE + i] = line[i];
         currentTileLine++;
     }
-    //printf("Parsed\n");
 
     free(line);
     return res;
@@ -210,14 +166,9 @@ void parseLine(char *line)
 {
     *strchr(line, '\n') = '\0';
     char *ptr;
-    //printf ("Parsing line %s", line);
     while ((ptr = strrchr(line, '#')) != NULL)
         *ptr = 1;
     while ((ptr = strrchr(line, '.')) != NULL)
         *ptr = 0;
-    /*printf(" to ");
-    for (int i = 0; i < SIZE; i++)
-        printf("%d", line[i]);
-    printf("\n");*/
 }
 
