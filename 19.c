21d01bda860c948a403bcb44b39bf9a45fb51fa0
metacolon <timo@teemze.de>
2020-12-21 15:20:23 +0100

    Cleanup and fix memory leaks

--- 19.c
+++ 19.c
@@ -38,20 +38,32 @@ int main()
 	FILE *in = fopen("in19", "r");
 
     rule *rules = parseRules(in);
-    //for (int i = 0; i < RULE_CNT; i++)
-        //printRule(rules[i]);
     rule rule0 = *rules;
     printf("Part 1: %d\n", getValidCount(rule0, in));
-    //for (int i = 0; i < RULE_CNT; i++)
-        //printRule(rules[i]);
+    for (int i = 0; i < RULE_CNT; i++)
+    {
+        if (rules[i].sub == NULL)
+            continue;
+        for (int j = 0; j < SUB_CNT; j++)
+            free(rules[i].sub[j]);
+        free(rules[i].sub);
+    }
+    free(rules);
     rewind(in);
     part2 = 1;
     rules = parseRules(in);
-    //for (int i = 0; i < RULE_CNT; i++)
-        //printRule(rules[i]);
     rule0 = *rules;
     printf("Part 2: %d\n", getValidCount(rule0, in));
-
+    
+    for (int i = 0; i < RULE_CNT; i++)
+    {
+        if (rules[i].sub == NULL)
+            continue;
+        for (int j = 0; j < SUB_CNT; j++)
+            free(rules[i].sub[j]);
+        free(rules[i].sub);
+    }
+    free(rules);
 	fclose(in);	
 	return 0;	
 }
@@ -62,10 +74,7 @@ void printRule(rule r)
         return;
     printf("Rule %3d", r.index);
     if (r.atomic)
-    {
-        //printf("\tAtomic: %c\n", r.atomic);
         return;
-    }
 
     printf("\t");
     for (int i = 0; i < SUB_CNT; i++)
@@ -92,7 +101,6 @@ int getValidCount(rule r, FILE *in)
     while (getline(&line, &n, in) > 0 && *line != '\n' && *line != '\0')
     {
         *strchr(line, '\n') = '\0';
-        //printf("Validating %s\n", line);
         res += apply(r, line);
     }
     free(line);
@@ -101,17 +109,11 @@ int getValidCount(rule r, FILE *in)
 
 char **applyAll(rule r, char *lineptr, int depth)
 {
-    //if (r.index == 8 || r.index == 11 || r.index == 31 || r.index == 42)
-        //printf("%d: Applying rule %d on %s\n", depth, r.index, lineptr);
     char **res = calloc(BRANCH_CNT, sizeof(*res));
     if (*lineptr == '\0')
-    {
-        //printf("Reached end of line\n");
         return res;
-    }
     if (r.atomic)
     {
-        //printf("Atomic: %c (%s)\n", r.atomic, lineptr);
         if (r.atomic == *lineptr)
             res[0] = lineptr+1;
         return res;
@@ -128,23 +130,18 @@ char **applyAll(rule r, char *lineptr, int depth)
             char **new = calloc(BRANCH_CNT, sizeof(*new));
             for (int k = 0; curr[k] != NULL; k++)
             {
-                //printf("Applying sub (%d, %d, %d)\n", i, j, k);
                 char **sub = applyAll(*r.sub[i][j], curr[k], depth+1);
-                //printf("Applied (%d, %d, %d) on %s\n", i, j, k, curr[k]);
                 memcpy(new+newIndex, sub, (BRANCH_CNT - newIndex) * sizeof(*new));
-                //printf("Copied - newIndex: %d (%s)\n", newIndex, new[newIndex]);
+                free(sub);
                 for (; new[newIndex] != NULL; newIndex++);
-                    //printf("Not null (%d) - %s\n", newIndex, new[newIndex]);
-                //printf("Afterwards\n");
             }
-            //printf("Updating curr\n");
             memset(curr, 0, BRANCH_CNT * sizeof(*curr));
             memcpy(curr, new, BRANCH_CNT * sizeof(*curr));
+            free(new);
         }
 
-        //printf("Done for one\n");
-
         memcpy(res+resIndex, curr, (BRANCH_CNT - resIndex) * sizeof(*res));
+        free(curr);
         for (; res[resIndex] != NULL; resIndex++);
     }
     return res;
@@ -152,17 +149,14 @@ char **applyAll(rule r, char *lineptr, int depth)
 
 int apply(rule r, char *line)
 {
-    //printf("Validating line %s\n", line);
     char **applied = applyAll(r, line, 0);
     for (int i = 0; applied[i] != NULL; i++)
-    {
         if (applied[i][0] == '\0')
         {
-            //printf("Line %s is valid\n", line);
+            free(applied);
             return 1;
         }
-    }
-    //printf("Line %s is invalid\n", line);
+    free(applied);
     return 0;
 }
 
@@ -193,7 +187,6 @@ void printRawRule(rawRule rule)
 
 rule *parseRules(FILE *in)
 {
-    //printf("Parsing\n");
     rawRule *raw = parseRawRules(in);
     rule *res = calloc(RULE_CNT, sizeof(*res));
     for (int i = 0; i < RULE_CNT; i++)
@@ -218,7 +211,15 @@ rule *parseRules(FILE *in)
                 res[i].sub[j][k] = res+raw[i].sub[j][k];
         }
     }
-    //printf("Parsed\n");
+    for (int i = 0; i < RULE_CNT; i++)
+    {
+        if (raw[i].sub == NULL)
+            continue;
+        for (int j = 0; j < SUB_CNT; j++)
+            free(raw[i].sub[j]);
+        free(raw[i].sub);
+    }
+    free(raw);
     return res;
 }
 
@@ -229,17 +230,10 @@ rawRule *parseRawRules(FILE *in)
     rawRule *res = malloc(RULE_CNT * sizeof(*res));
     while (getline(&line, &n, in) > 0 && *line != '\n' && *line != '\0')
     {
-        //printf("Raw line: %s", line);
         *strchr(line, '\n') = '\0';
-        //printf("Line: %s\n", line);
-        //printf("Parsing line %s\n", line);
         rawRule parsed = parseRule(line);
-        //printf("Parsed - index: %d; atomic: %d\n", parsed.index, parsed.atomic);
         res[parsed.index] = parsed;
-        //printf("Assgined\n");
-        //printRawRule(parsed);
     }
-    //printf("Parsed everything\n");
 
     free(line);
     return res;
@@ -247,52 +241,48 @@ rawRule *parseRawRules(FILE *in)
 
 rawRule parseRule(char *line)
 {
+    char *dupped = NULL;
     if (part2 && line[0] == '8' && line[1] == ':')
+    {
         line = strdup("8: 42 | 42 8");
+        dupped = line;
+    }
     else if (part2 && line[0] == '1' && line[1] == '1' && line[2] == ':')
+    {
         line = strdup("11: 42 31 | 42 11 31");
+        dupped = line;
+    }
     rawRule res = {0};
     char *find = strchr(line, ':');
     *find = '\0';
     res.index = atoi(line);
-    //printf("Got index %d\n", res.index);
     line = find+2;;
 
     find = strchr(line, '"');
     if (find != NULL)
     {
-        //printf("Found atomic\n");
         res.atomic = find[1];
+        if (dupped)
+            free(dupped);
         return res;
     }
 
     res.sub = calloc(SUB_CNT, sizeof(*res.sub));
-    //printf("Line: %s\n", line);
     find = strtok_r(line, "|", &line);
     for (int i = 0; find != NULL /*&& find[0] != '\0'*/; i++, find = strtok_r(NULL, "|", &line))
     {
-        //printf("Find: '%s', %d\n", find, i);
         res.sub[i] = calloc(SUB_CNT, sizeof(*res.sub[i]));
         char *token;
         int j;
         token = strtok_r(find, " ", &find);
         for (j = 0; token != NULL && token[0] != '\0'; j++, token = strtok_r(NULL, " ", &find))
-        {
-            //printf("Token: %s; i: %d; j: %d\n", token, i, j);
             res.sub[i][j] = atoi(token);
-            //printf("Assigned\n");
-        }
         for (; j < SUB_CNT; j++)
-        {
-            //printf("Clearing %d\n", j);
             res.sub[i][j] = -1;
-        }
-
-        //printf("Onto the next!\n");
     }
 
-    //printf("Aaaand we're done\n");
-
+    if (dupped)
+        free(dupped);
     return res;
 }
 
